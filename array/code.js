2a81e94d77962f7612c7604a1a2f2ed7d331c554
Prince Babu <princebabu4495@gmail.com>
2026-04-27 23:55:19 +0530

    feat : practice remove duplicates and sort elements and count elements ?

---
+++ array/code.js
@@ -0,0 +1,36 @@
+const arr = [
+  10, 80, 30, 40, 50, 60, 70, 20, 90, 100, 50, 30, 20, 10, 80, 30, 40, 50, 60,
+  70, 20, 90, 100, 50, 30, 20, 10, 80, 30, 40, 50, 60, 70, 20, 90, 100,
+];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(`${i} : ${arr[i]}`);
+// }
+
+let sum = 0;
+
+arr.forEach((elem) => {
+  sum += elem;
+});
+
+// console.log(sum);
+
+// remove duplicates and sort in descending order
+const newArr = [...new Set(arr)].sort((a, b) => b - a);
+
+// count the duplicates
+
+const count = {};
+
+const newArr2 = [1, 2, 2, 3, 4, 5, 5, 6, 7];
+
+newArr2.forEach((elem) => {
+  if (count[elem]) {
+    // if the element already exists in the count object, increment its count
+    count[elem]++;
+  } else {
+    count[elem] = 1; // if the element does not exist in the count object, initialize its count to 1
+  }
+});
+
+console.log(count);
